515fa7c6fc4b2ae2faf27a7be9c8354c490140b3
ff-131867 <ff-131867@ap055027.hs-weingarten.de>
2016-04-25 11:09:13 +0200

    play around with kmalloc

--- drivers/misc/ffrick.c
+++ drivers/misc/ffrick.c
@@ -1,14 +1,32 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/slab.h>
+#include <asm/io.h>
+
+static u8* ffrick_mem;
 
 static void __exit ffrick_exit (void)
 {
-	printk(KERN_INFO "ffrick_exit function done");
+	printk(KERN_INFO "%s\n", __func__);
+	
+	if (ffrick_mem)
+		kfree(ffrick_mem);
 }
 
 static int __init ffrick_init (void)
 {
-	printk(KERN_INFO "ffrick_init function done");
+	unsigned int i;
+	
+	ffrick_mem = kmalloc(64, GFP_KERNEL);
+
+	printk(KERN_INFO "ffrick_mem: %p - %p\n",
+		ffrick_mem, (void *) virt_to_phys(ffrick_mem));
+
+	for (i = 0; i < 64; i++) {
+		ffrick_mem[i] = 0;
+	}
+	ffrick_mem[0] = 1;
+	printk(KERN_INFO "ffrick init function done");
 	return 0;
 }
 
